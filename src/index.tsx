b27004697604a2c65a9190896f2c82cf08d5ea75
Ray Zane <raymondzane@gmail.com>
2019-06-27 16:04:06 -0400

    Fix input

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,3 @@
-import * as React from "react";
-
-// Delete me
-export const Thing = () => {
-  return <div>the snozzberries taste like snozzberries</div>;
-};
+export * from "./Checkbox";
+export * from "./DateTimeInput";
+export * from "./Select";
