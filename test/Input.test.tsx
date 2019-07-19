4080a3956a943e6ccd7166c4454e0f21410c2d47
Ray Zane <raymondzane@gmail.com>
2019-07-19 14:38:32 -0400

    Test naming

--- test/Input.test.tsx
+++ test/Input.test.tsx
@@ -2,25 +2,27 @@ import React from "react";
 import { shallow } from "enzyme";
 import { Input } from "../src";
 
-it("renders", () => {
-  const onChange = jest.fn();
-  const input = shallow(<Input value="hello" onChange={onChange} />);
+describe("<Input />", () => {
+  it("renders", () => {
+    const onChange = jest.fn();
+    const input = shallow(<Input value="hello" onChange={onChange} />);
 
-  expect(input).toMatchSnapshot();
-});
+    expect(input).toMatchSnapshot();
+  });
 
-it("emits a value on change", () => {
-  const onChange = jest.fn();
-  const input = shallow(<Input value="hello" onChange={onChange} />);
+  it("emits a value on change", () => {
+    const onChange = jest.fn();
+    const input = shallow(<Input value="hello" onChange={onChange} />);
 
-  input.find("TextInput").simulate("changeText", "goodbye");
-  expect(onChange).toHaveBeenCalledWith("goodbye");
-});
+    input.find("TextInput").simulate("changeText", "goodbye");
+    expect(onChange).toHaveBeenCalledWith("goodbye");
+  });
 
-it("emits a `null` value on change when the text is blank", () => {
-  const onChange = jest.fn();
-  const input = shallow(<Input value="hello" onChange={onChange} />);
+  it("emits a `null` value on change when the text is blank", () => {
+    const onChange = jest.fn();
+    const input = shallow(<Input value="hello" onChange={onChange} />);
 
-  input.find("TextInput").simulate("changeText", " ");
-  expect(onChange).toHaveBeenCalledWith(null);
+    input.find("TextInput").simulate("changeText", " ");
+    expect(onChange).toHaveBeenCalledWith(null);
+  });
 });
--- test/IntegerInput.test.tsx
+++ test/IntegerInput.test.tsx
@@ -2,22 +2,24 @@ import React from "react";
 import { shallow } from "enzyme";
 import { IntegerInput } from "../src";
 
-it("renders", () => {
-  const onChange = jest.fn();
-  const input = shallow(<IntegerInput value={5} onChange={onChange} />);
-  expect(input).toMatchSnapshot();
-});
+describe('<IntegerInput />', () => {
+  it("renders", () => {
+    const onChange = jest.fn();
+    const input = shallow(<IntegerInput value={5} onChange={onChange} />);
+    expect(input).toMatchSnapshot();
+  });
 
-it("emits an integer value on change", () => {
-  const onChange = jest.fn();
-  const input = shallow(<IntegerInput value={null} onChange={onChange} />);
-  input.find("TextInput").simulate("changeText", "aaaa1aaaaa");
-  expect(onChange).toHaveBeenCalledWith(1);
-});
+  it("emits an integer value on change", () => {
+    const onChange = jest.fn();
+    const input = shallow(<IntegerInput value={null} onChange={onChange} />);
+    input.find("TextInput").simulate("changeText", "aaaa1aaaaa");
+    expect(onChange).toHaveBeenCalledWith(1);
+  });
 
-it("emits a `null` value when the input cannot be parsed", () => {
-  const onChange = jest.fn();
-  const input = shallow(<IntegerInput value={null} onChange={onChange} />);
-  input.find("TextInput").simulate("changeText", "aaaaa");
-  expect(onChange).toHaveBeenCalledWith(null);
+  it("emits a `null` value when the input cannot be parsed", () => {
+    const onChange = jest.fn();
+    const input = shallow(<IntegerInput value={null} onChange={onChange} />);
+    input.find("TextInput").simulate("changeText", "aaaaa");
+    expect(onChange).toHaveBeenCalledWith(null);
+  });
 });
