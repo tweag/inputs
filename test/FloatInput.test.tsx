b5e1dda7428732748fb25769fee601f216d61cf6
Ray Zane <raymondzane@gmail.com>
2019-07-19 14:54:44 -0400

    Add a test for FloatInput

---
+++ test/FloatInput.test.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { FloatInput } from "../src";
+import { shallow } from "enzyme";
+
+describe("<FloatInput />", () => {
+  it("renders", () => {
+    const onChange = jest.fn();
+    const input = shallow(<FloatInput value={5} onChange={onChange} />);
+    expect(input).toMatchSnapshot();
+  });
+
+  it("emits an integer value on change", () => {
+    const onChange = jest.fn();
+    const input = shallow(<FloatInput value={null} onChange={onChange} />);
+    input.find("DecimalInput").simulate("change", "1");
+    expect(onChange).toHaveBeenCalledWith(1);
+  });
+
+  it("emits an float value on change", () => {
+    const onChange = jest.fn();
+    const input = shallow(<FloatInput value={null} onChange={onChange} />);
+    input.find("DecimalInput").simulate("change", "1.5");
+    expect(onChange).toHaveBeenCalledWith(1.5);
+  });
+
+  it("emits a `null` for `null` values", () => {
+    const onChange = jest.fn();
+    const input = shallow(<FloatInput value={null} onChange={onChange} />);
+    input.find("DecimalInput").simulate("change", null);
+    expect(onChange).toHaveBeenCalledWith(null);
+  });
+});
