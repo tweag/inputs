9bbc976a4cfb156cd56bb98a1b9c099131e43124
Ray Zane <raymondzane@gmail.com>
2019-07-21 12:53:52 -0400

    Configure

---
+++ example/App.tsx
@@ -0,0 +1,10 @@
+import "./App.css";
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return <h1>Formik Inputs</h1>;
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
