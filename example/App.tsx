2ae2c9bc0fc99fa36ba23dbd36da486653bae807
Ray Zane <raymondzane@gmail.com>
2019-07-21 13:20:15 -0400

    Simplest working example

--- example/App.tsx
+++ example/App.tsx
@@ -2,9 +2,50 @@ import "./App.css";
 
 import React from "react";
 import ReactDOM from "react-dom";
+import { Formik, Form } from "formik";
+import { Input } from "../src/";
+
+interface Values {
+  string: string | null;
+  select: string | null;
+  integer: number | null;
+  float: number | null;
+  date: string | null;
+  datetime: string | null;
+  boolean: boolean | null;
+  file: File | null;
+  files: FileList | null;
+}
+
+const initialValues: Values = {
+  string: null,
+  select: null,
+  integer: null,
+  float: null,
+  date: null,
+  datetime: null,
+  boolean: null,
+  file: null,
+  files: null
+};
+
+const onSubmit = (values: Values) => {
+  console.log(values);
+};
 
 const App = () => {
-  return <h1>Formik Inputs</h1>;
+  return (
+    <div className="App">
+      <h1>Formik Inputs</h1>
+
+      <Formik onSubmit={onSubmit} initialValues={initialValues}>
+        <Form>
+          <Input name="string" />
+          <button type="submit">Submit</button>
+        </Form>
+      </Formik>
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.getElementById("root"));
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,16 @@
-import * as React from 'react';
+import * as React from "react";
+import { Field, FieldAttributes } from "formik";
+import { Input as BaseInput } from "react-baseline-inputs";
 
-// Delete me
-export const Thing = () => {
-  return <div>the snozzberries taste like snozzberries</div>;
+export const Input = (props: FieldAttributes<any>) => {
+  return (
+    <Field {...props}>
+      {({ field, form }: any) => (
+        <BaseInput
+          {...field}
+          onChange={value => form.setFieldValue(field.name, value)}
+        />
+      )}
+    </Field>
+  );
 };
