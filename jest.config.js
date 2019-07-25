33a9d0504b89872fa3e7f614f3b1ce098062eb45
Ray Zane <raymondzane@gmail.com>
2019-07-25 12:00:39 -0400

    Configure React Native

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+const { defaults: preset } = require("ts-jest/presets");
+
+module.exports = {
+  ...preset,
+  preset: "react-native",
+  snapshotSerializers: ["enzyme-to-json/serializer"],
+  setupFilesAfterEnv: ["<rootDir>/test/setup.ts"],
+  collectCoverageFrom: ["<rootDir>/packages/**/*.{ts,tsx}"],
+  testMatch: ["<rootDir>/packages/**/*.test.{ts,tsx}"],
+  modulePathIgnorePatterns: ["<rootDir>/apps/"],
+  transform: {
+    ...preset.transform,
+    "\\.js$": "<rootDir>/node_modules/react-native/jest/preprocessor.js"
+  },
+  // globals: {
+  //   "ts-jest": {
+  //     tsConfig: "tsconfig.jest.json"
+  //   }
+  // }
+};
