c3703f025f116d967037535b35b4704b740bad81
Ray Zane <raymondzane@gmail.com>
2019-08-01 17:09:28 -0400

    fix: remove commented out code

--- jest.config.js
+++ jest.config.js
@@ -12,9 +12,4 @@ module.exports = {
     ...preset.transform,
     "\\.js$": "<rootDir>/node_modules/react-native/jest/preprocessor.js"
   }
-  // globals: {
-  //   "ts-jest": {
-  //     tsConfig: "tsconfig.jest.json"
-  //   }
-  // }
 };
