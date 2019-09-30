ee872fd937823e275975f48fd75d732040ae3802
Ray Zane <raymondzane@gmail.com>
2019-09-30 17:54:53 -0400

    fix: allow the user to type a date

--- src/DateInput.tsx
+++ src/DateInput.tsx
@@ -1,8 +1,5 @@
 import * as React from "react";
 import { CustomInputProps } from "./types";
-import format from "date-fns/format";
-import parse from "date-fns/parse";
-import isValid from "date-fns/is_valid";
 
 export type DateInputProps = CustomInputProps<"input", string | null>;
 
@@ -17,30 +14,17 @@ export const DateInput: React.FC<DateInputProps> = ({
   onChange,
   ...props
 }) => {
-  const formattedValue = React.useMemo(
-    () => (value ? format(value, "YYYY-MM-DD") : ""),
-    [value]
-  );
-
   const handleChange = React.useCallback(
     event => {
-      const value = parse(event.target.value);
-
-      if (isValid(value)) {
-        onChange(format(value, "YYYY-MM-DD"));
-      } else {
-        onChange(null);
-      }
+      const { value } = event.target;
+      const date = new Date(value);
+      const isValid = !isNaN(date.valueOf());
+      onChange(isValid ? value : null);
     },
     [onChange]
   );
 
   return (
-    <input
-      {...props}
-      type="date"
-      value={formattedValue}
-      onChange={handleChange}
-    />
+    <input {...props} type="date" value={value || ""} onChange={handleChange} />
   );
 };
