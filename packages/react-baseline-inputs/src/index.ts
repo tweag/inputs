c8bc5a7a2d9b5aa1b26ceffc0888c09879012898
Ray Zane <raymondzane@gmail.com>
2019-10-11 16:43:16 -0400

    chore: rename file

--- packages/react-baseline-inputs/src/index.ts
+++ packages/react-baseline-inputs/src/index.ts
@@ -3,7 +3,7 @@ export * from "./DateInput";
 export * from "./DateTimeInput";
 export * from "./FileInput";
 export * from "./FloatInput";
-export * from "./Input";
 export * from "./IntegerInput";
 export * from "./Select";
 export * from "./TextArea";
+export * from "./TextInput";
