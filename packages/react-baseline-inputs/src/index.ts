fe8c0c5cc7f68dbe14f3c714c56c0901d7fea4e9
Ray Zane <raymondzane@gmail.com>
2019-10-11 17:56:14 -0400

    feat: export the field

--- packages/react-baseline-inputs/src/index.ts
+++ packages/react-baseline-inputs/src/index.ts
@@ -1,4 +1,5 @@
 export * from "./Checkbox";
+export * from "./Field";
 export * from "./DateInput";
 export * from "./DateTimeInput";
 export * from "./FileInput";
--- packages/react-baseline-inputs/test/index.test.ts
+++ packages/react-baseline-inputs/test/index.test.ts
@@ -3,6 +3,7 @@ import * as inputs from "../src";
 test("exports", () => {
   expect(Object.keys(inputs).sort()).toEqual([
     "Checkbox",
+    "Field",
     "DateInput",
     "DateTimeInput",
     "FileInput",
