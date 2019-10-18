d83e2bedf1bae4338442192faa99fae450d8ace5
Ray Zane <raymondzane@gmail.com>
2019-10-18 17:34:49 -0400

    chore: blacklist the use of multiple

--- src/types.ts
+++ src/types.ts
@@ -43,7 +43,10 @@ export interface ValueProps<V> {
   onChange: (value: V) => void;
 }
 
-type HTMLProps<T> = Omit<React.HTMLProps<T>, "value" | "onChange" | "label">;
+type HTMLProps<T> = Omit<
+  React.HTMLProps<T>,
+  "value" | "onChange" | "label" | "multiple"
+>;
 type InputProps<V, E = HTMLInputElement> = FieldInputProps &
   HTMLProps<E> &
   ValueProps<V>;
