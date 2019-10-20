6b884fb141e3aec8c0c699165ae0eeb60feeb990
Ray Zane <raymondzane@gmail.com>
2019-10-20 15:41:18 -0400

    feat: add a theme prop

---
+++ packages/react-baseline-inputs/src/theme.ts
@@ -0,0 +1,30 @@
+import { Theme } from "./types";
+
+export const defaultTheme: Theme = {
+  field: "field",
+  fieldInline: "field--inline",
+  fieldSuccess: "field--success",
+  fieldError: "field--erroneous",
+  fieldTouched: "field--touched",
+  fieldLarge: "field--large",
+  fieldSmall: "field--small",
+
+  input: "field__input",
+  inputInline: "field__input--inline",
+  inputSuccess: "field__input--success",
+  inputError: "field__input--erroneous",
+  inputTouched: "field__input--touched",
+  inputLarge: "field__input--large",
+  inputSmall: "field__input--small",
+
+  label: "field__label",
+  labelInline: "field__label--inline",
+  labelSuccess: "field__label--success",
+  labelError: "field__label--error",
+  labelTouched: "field__label--touched",
+  labelLarge: "field__label--large",
+  labelSmall: "field__label--small",
+
+  help: "field__help",
+  error: "message message--problem"
+};
