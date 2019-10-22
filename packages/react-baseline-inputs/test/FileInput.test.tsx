e45da467d985353cc074e3ea2539016308376c1e
Ray Zane <raymondzane@gmail.com>
2019-10-22 13:40:26 -0400

    feat: test with jest-axe

--- packages/react-baseline-inputs/test/FileInput.test.tsx
+++ packages/react-baseline-inputs/test/FileInput.test.tsx
@@ -1,35 +1,17 @@
 import * as React from "react";
-import { FileInput, FileInputProps } from "../src";
 import { render, fireEvent } from "@testing-library/react";
+import { FileInput, FileInputProps } from "../src";
+import { itBehavesLikeAField } from "./sharedExamples";
 
 const setup = (props: Partial<FileInputProps> = {}) =>
   render(<FileInput label="Jawn" onChange={jest.fn()} {...props} />);
 
 describe("<FileInput />", () => {
-  const file = Symbol("File");
-
-  it("renders", () => {
-    const { container } = setup();
-    expect(container.firstChild).toMatchSnapshot();
-  });
-
-  it("renders without a label", () => {
-    const { container } = setup({ label: false });
-    expect(container.firstChild).toMatchSnapshot();
-  });
-
-  it("renders without a wrapper", () => {
-    const { container } = setup({ label: false, wrapper: false });
-    expect(container.firstChild).toMatchSnapshot();
-  });
-
-  it("renders with an error", () => {
-    const { container } = setup({ error: "Oh no!" });
-    expect(container.firstChild).toMatchSnapshot();
-  });
+  itBehavesLikeAField(setup);
 
   it("emits an instance of File", () => {
     const onChange = jest.fn();
+    const file = Symbol("File");
     const { getByLabelText } = setup({ onChange });
 
     fireEvent.change(getByLabelText("Jawn"), {
--- packages/react-baseline-inputs/test/FileListInput.test.tsx
+++ packages/react-baseline-inputs/test/FileListInput.test.tsx
@@ -1,35 +1,17 @@
 import * as React from "react";
-import { FileListInput, FileListInputProps } from "../src";
 import { render, fireEvent } from "@testing-library/react";
+import { itBehavesLikeAField } from "./sharedExamples";
+import { FileListInput, FileListInputProps } from "../src";
 
 const setup = (props: Partial<FileListInputProps> = {}) =>
   render(<FileListInput label="Jawn" onChange={jest.fn()} {...props} />);
 
 describe("<FileListInput />", () => {
-  const files = Symbol("FileList");
-
-  it("renders", () => {
-    const { container } = setup();
-    expect(container.firstChild).toMatchSnapshot();
-  });
-
-  it("renders without a label", () => {
-    const { container } = setup({ label: false });
-    expect(container.firstChild).toMatchSnapshot();
-  });
-
-  it("renders without a wrapper", () => {
-    const { container } = setup({ label: false, wrapper: false });
-    expect(container.firstChild).toMatchSnapshot();
-  });
-
-  it("renders with an error", () => {
-    const { container } = setup({ error: "Oh no!" });
-    expect(container.firstChild).toMatchSnapshot();
-  });
+  itBehavesLikeAField(setup);
 
   it("emits an instance of FileList", () => {
     const onChange = jest.fn();
+    const files = Symbol("FileList");
     const { getByLabelText } = setup({ onChange });
 
     fireEvent.change(getByLabelText("Jawn"), {
