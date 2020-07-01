ea68a8016d61e1fcf8a46808934182986a4fe80c
Ray Zane <raymondzane@gmail.com>
2020-07-01 14:29:12 -0400

    Implement theme functions

--- src/theme/bootstrapTheme.ts
+++ src/theme/bootstrapTheme.ts
@@ -1,56 +1,48 @@
-import { FieldTheme, Theme } from "../types";
+import cc from "classcat";
+import { ThemeContext, Theme } from "../types";
 
-const fieldTheme: FieldTheme = {
-  field: "form-group",
-  input: "form-control",
-  inputSuccess: "is-valid",
-  inputError: "is-invalid",
-  inputSmall: "form-control-sm",
-  inputLarge: "form-control-lg",
-  error: "invalid-feedback",
-  help: "form-text text-muted"
-};
+export const bootstrapTheme = (ctx: ThemeContext): Theme => {
+  const isCheckbox = ctx.type === "checkbox";
+  const isSelect = ctx.type === "select";
+  const isToggle = ctx.type === "toggle-button";
+  const isFile = ["file", "file-list"].includes(ctx.type);
 
-const checkboxTheme: FieldTheme = {
-  ...fieldTheme,
-  field: "form-check",
-  fieldInline: "form-check-inline",
-  input: "form-check-input",
-  label: "form-check-label"
-};
+  let label: string | undefined = undefined;
 
-const selectTheme: FieldTheme = {
-  ...fieldTheme,
-  input: "custom-select"
-};
+  const field: Record<string, boolean> = {
+    "form-group": true
+  };
 
-const fileInputTheme: FieldTheme = {
-  ...fieldTheme,
-  field: "custom-file",
-  input: "custom-file-input",
-  label: "custom-file-label"
-};
+  const input: Record<string, boolean> = {
+    "is-valid": ctx.valid,
+    "is-invalid": !ctx.valid
+  };
 
-const toggleButtonTheme: FieldTheme = {
-  ...fieldTheme,
-  field: "custom-control custom-switch",
-  input: "custom-control-input",
-  label: "custom-control-label"
-};
+  if (isCheckbox) {
+    label = "form-check-label";
+    field["form-check"] = true;
+    input["form-check-input"] = true;
+  } else if (isSelect) {
+    input["custom-select"] = true;
+    input["custom-select-sm"] = ctx.small;
+    input["custom-select-lg"] = ctx.large;
+  } else if (isToggle) {
+    label = "custom-control-label";
+    field["custom-control"] = true;
+    field["custom-switch"] = true;
+    input["custom-control-input"] = true;
+  } else {
+    input["form-control"] = !isFile;
+    input["form-control-file"] = isFile;
+    input["form-control-sm"] = ctx.small;
+    input["form-control-lg"] = ctx.large;
+  }
 
-export const bootstrapTheme: Theme = {
-  checkbox: checkboxTheme,
-  dateInput: fieldTheme,
-  dateTimeInput: fieldTheme,
-  field: fieldTheme,
-  fieldSet: {},
-  fileInput: fileInputTheme,
-  floatInput: fieldTheme,
-  input: fieldTheme,
-  integerInput: fieldTheme,
-  radioGroup: checkboxTheme,
-  select: selectTheme,
-  textarea: fieldTheme,
-  timeInput: fieldTheme,
-  toggleButton: toggleButtonTheme
+  return {
+    label,
+    error: "invalid-feedback",
+    help: "form-text text-muted",
+    field: cc(field),
+    input: cc(input)
+  };
 };
--- src/theme/defaultTheme.ts
+++ src/theme/defaultTheme.ts
@@ -1,89 +1,25 @@
-import { FieldTheme, FieldSetTheme, Theme } from "../types";
+import cc from "classcat";
+import { Theme, ThemeContext } from "../types";
 
-const fieldTheme: FieldTheme = {
-  field: "field",
-  fieldInline: "field--inline",
-  fieldCondensed: "field--condensed",
-  fieldSuccess: "field--success",
-  fieldError: "field--erroneous",
-  fieldTouched: "field--touched",
-  fieldPopulated: "field--populated",
-  fieldLarge: "field--large",
-  fieldSmall: "field--small",
-  fieldDisabled: "field--disabled",
+const build = (prefix: string, ctx: ThemeContext) => ({
+  [prefix]: true,
+  [`${prefix}--${ctx.type}`]: true,
+  [`${prefix}--inline`]: ctx.inline,
+  [`${prefix}--condensed`]: ctx.condensed,
+  [`${prefix}--success`]: ctx.valid,
+  [`${prefix}--erroneous`]: !ctx.valid,
+  [`${prefix}--touched`]: ctx.touched,
+  [`${prefix}--populated`]: ctx.populated,
+  [`${prefix}--large`]: ctx.large,
+  [`${prefix}--small`]: ctx.small
+});
 
-  input: "field__input",
-  inputInline: "field__input--inline",
-  inputCondensed: "field__input--condensed",
-  inputSuccess: "field__input--success",
-  inputError: "field__input--erroneous",
-  inputTouched: "field__input--touched",
-  inputPopulated: "field__input--populated",
-  inputLarge: "field__input--large",
-  inputSmall: "field__input--small",
-  inputDisabled: "field__input--disabled",
-
-  label: "field__label",
-  labelInline: "field__label--inline",
-  labelCondensed: "field__label--condensed",
-  labelSuccess: "field__label--success",
-  labelError: "field__label--erroneous",
-  labelTouched: "field__label--touched",
-  labelPopulated: "field__label--populated",
-  labelLarge: "field__label--large",
-  labelSmall: "field__label--small",
-  labelDisabled: "field__label--disabled",
-
-  help: "field__help",
-  helpInline: "field__help--inline",
-  helpCondensed: "field__help--condensed",
-  helpSmall: "field__help--small",
-  helpLarge: "field__help--large",
-
-  error: "field__error",
-  errorInline: "field__error--inline",
-  errorCondensed: "field__error--condensed",
-  errorSmall: "field__error--small",
-  errorLarge: "field__error--large"
-};
-
-const fieldSetTheme: FieldSetTheme = {
-  fieldSet: "fieldset",
-  fieldSetError: "fieldset--erroneous",
-  fieldSetSuccess: "fieldset--success",
-  fieldSetTouched: "fieldset--touched",
-  fieldSetSmall: "fieldset--small",
-  fieldSetLarge: "fieldset--large",
-
-  legend: "fieldset__legend",
-  legendError: "fieldset__legend--erroneous",
-  legendSuccess: "fieldset__legend--success",
-  legendTouched: "fieldset__legend--touched",
-  legendSmall: "fieldset__legend--small",
-  legendLarge: "fieldset__legend--large",
-
-  help: "fieldset__help",
-  helpSmall: "fieldset__help--small",
-  helpLarge: "fieldset__help--large",
-
-  error: "fieldset__error",
-  errorSmall: "fieldset__error--small",
-  errorLarge: "fieldset__error--large"
-};
-
-export const defaultTheme: Theme = {
-  input: fieldTheme,
-  checkbox: fieldTheme,
-  dateInput: fieldTheme,
-  dateTimeInput: fieldTheme,
-  field: fieldTheme,
-  fieldSet: fieldSetTheme,
-  fileInput: fieldTheme,
-  floatInput: fieldTheme,
-  integerInput: fieldTheme,
-  radioGroup: fieldTheme,
-  select: fieldTheme,
-  textarea: fieldTheme,
-  timeInput: fieldTheme,
-  toggleButton: fieldTheme
-};
+export const defaultTheme = (ctx: ThemeContext): Theme => ({
+  field: cc(build("field", ctx)),
+  input: cc(build("field__input", ctx)),
+  label: cc(build("field__label", ctx)),
+  help: cc(build("field__help", ctx)),
+  error: cc(build("field__error", ctx)),
+  fieldset: cc(build("fieldset", ctx)),
+  legend: cc(build("fieldset__legend", ctx))
+});
