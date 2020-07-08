056827a195140e3bc026aa00cb474c6f03a537f4
Ray Zane <raymondzane@gmail.com>
2020-07-08 17:36:09 -0400

    Add tests for textarea

---
+++ test/TextArea.test.tsx
@@ -0,0 +1,30 @@
+import * as React from "react";
+import { TextArea, TextAreaProps } from "../src";
+import { render, fireEvent } from "@testing-library/react";
+import { itBehavesLikeAField } from "./sharedExamples";
+
+function setup(props: Partial<TextAreaProps> = {}) {
+  return render(<TextArea onChange={() => null} value="" {...props} />);
+}
+
+describe("<TextArea />", () => {
+  itBehavesLikeAField(setup);
+
+  it("has a value", () => {
+    const field = setup({ value: "foo" });
+    const input = field.getByRole("textbox");
+    expect(input).toHaveValue("foo");
+  });
+
+  it("emits `onChange`", () => {
+    const onChange = jest.fn();
+    const field = setup({ onChange });
+    const input = field.getByRole("textbox");
+
+    fireEvent.change(input, {
+      target: { value: "hi" }
+    });
+
+    expect(onChange).toHaveBeenCalledWith("hi");
+  });
+});
