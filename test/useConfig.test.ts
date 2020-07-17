cab5506d3246f089ec8155d6af25172297e90116
Ray Zane <raymondzane@gmail.com>
2020-07-17 00:24:37 -0400

    Add tests for `useConfig`

--- test/useConfig.test.ts
+++ test/useConfig.test.ts
@@ -1,4 +1,11 @@
-import { Theme, merge } from "../src";
+import { Config, FieldProps, Theme, merge } from "../src";
+import { renderHook } from "@testing-library/react-hooks";
+import { useConfig } from "../src/useConfig";
+
+interface Props {
+  foo?: boolean;
+  bar?: boolean;
+}
 
 describe("merge", () => {
   it("combines two objects", () => {
@@ -29,5 +36,57 @@ describe("merge", () => {
     const a: Theme<{}> = () => ({ input: "a" });
     const b: Theme<{}> = { field: "b" };
     expect(merge(a, b)({})).toEqual({ field: "b", input: "a" });
+  });
+});
+
+describe("useConfig", () => {
+  const props: Props = { foo: true, bar: true };
+
+  it("deletes custom props", () => {
+    const config: Config<FieldProps, Props> = {
+      omit: ["foo"]
+    };
+
+    const { result } = renderHook(() =>
+      useConfig<FieldProps, Props>(config, props)
+    );
+
+    expect(result.current).toEqual({ bar: true });
+  });
+
+  it("loads props from a hook", () => {
+    const config: Config<FieldProps, Props> = {
+      hook: props => ({ touched: props.foo })
+    };
+
+    const { result } = renderHook(() =>
+      useConfig<FieldProps, Props>(config, props)
+    );
+
+    expect(result.current).toEqual({ ...props, touched: true });
+  });
+
+  it("injects a theme prop", () => {
+    const config: Config<FieldProps, Props> = {
+      theme: { input: "foo" }
+    };
+
+    const { result } = renderHook(() =>
+      useConfig<FieldProps, Props>(config, props)
+    );
+
+    expect(result.current).toEqual({ ...props, theme: { input: "foo" } });
+  });
+
+  it("injects a theme function", () => {
+    const config: Config<FieldProps, Props> = {
+      theme: props => ({ input: props.foo ? "foo" : undefined })
+    };
+
+    const { result } = renderHook(() =>
+      useConfig<FieldProps, Props>(config, props)
+    );
+
+    expect(result.current).toEqual({ ...props, theme: { input: "foo" } });
   });
 });
