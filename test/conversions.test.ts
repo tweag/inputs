91474f13c9e77c84575529a683b4f25452cec9bf
Ray Zane <raymondzane@gmail.com>
2020-08-02 03:42:08 -0400

    Fix tests

--- test/conversions.test.ts
+++ test/conversions.test.ts
@@ -2,25 +2,12 @@ import {
   formatDateTime,
   formatNumber,
   formatTime,
-  isValidDateTime,
-  isValidNumber,
-  isValidTime,
   parseDateTime,
   parseNumber,
   parseTime
 } from "../src";
 
 describe("number", () => {
-  describe("isValidNumber", () => {
-    test("accepts valid values", () => {
-      expect(isValidNumber("1")).toEqual(true);
-    });
-
-    test("rejects invalid values", () => {
-      expect(isValidNumber("")).toEqual(false);
-    });
-  });
-
   describe("formatNumber", () => {
     test("converts a number to a string", () => {
       expect(formatNumber(0)).toEqual("0");
@@ -37,27 +24,13 @@ describe("number", () => {
     });
 
     test("rejects invalid values", () => {
-      expect(() => parseNumber("")).toThrow(/Unable to convert/);
+      expect(parseNumber("")).toBeUndefined();
     });
   });
 });
 
 describe("time", () => {
   const today = new Date("July 8, 2020");
-
-  describe("isValidTime", () => {
-    test("accepts valid values", () => {
-      expect(isValidTime("08:00")).toEqual(true);
-    });
-
-    test("rejects invalid values", () => {
-      expect(isValidTime("")).toEqual(false);
-      expect(isValidTime(":")).toEqual(false);
-      expect(isValidTime("8:8")).toEqual(false);
-      expect(isValidTime("25:00")).toEqual(false);
-      expect(isValidTime("11:61")).toEqual(false);
-    });
-  });
 
   describe("formatTime", () => {
     test("converts UTC to local", () => {
@@ -79,11 +52,11 @@ describe("time", () => {
     });
 
     test("rejects invalid values", () => {
-      expect(() => parseTime("", today)).toThrow(/Unable to convert/);
+      expect(parseTime("", today)).toBeUndefined();
     });
 
     test("rejects out of range", () => {
-      expect(() => formatTime("25:99", today)).toThrow(/Unable to convert/);
+      expect(parseTime("25:99", today)).toBeUndefined();
     });
   });
 });
@@ -92,25 +65,13 @@ describe("datetime-local", () => {
   const localValue = "2020-07-08T12:30";
   const utcValue = "2020-07-08T16:30:00.000Z";
 
-  describe("isValidTime", () => {
-    test("accepts valid values", () => {
-      expect(isValidDateTime(localValue)).toEqual(true);
-    });
-
-    test("rejects invalid values", () => {
-      expect(isValidDateTime("")).toEqual(false);
-      expect(isValidDateTime(":")).toEqual(false);
-      expect(isValidDateTime("abc")).toEqual(false);
-    });
-  });
-
   describe("parseDateTime", () => {
     test("converts UTC to local", () => {
       expect(parseDateTime(localValue)).toEqual(utcValue);
     });
 
     test("rejects invalid values", () => {
-      expect(() => parseDateTime("abc")).toThrow(/Unable to convert/);
+      expect(parseDateTime("abc")).toBeUndefined();
     });
   });
 
